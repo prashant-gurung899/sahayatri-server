8e9d1deac9460024c8a4f37153a61d27f63d6241
Prashant Gurung <prasantgrg777@gmail.com>
2023-12-19 12:44:59 +0545

    trip

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
+import { TripModule } from './trip/trip.module';
 
 @Module({
-  imports: [PrismaModule, UserModule, AuthModule],
+  imports: [PrismaModule, UserModule, AuthModule, TripModule],
   controllers: [AppController],
   providers: [AppService],
 })
